447e1fdc71f3a91ce80cc4cb7ce683b4d4fdf96c
vishal-1codes <pawarvishal10k@gmail.com>
2022-03-10 22:56:46 +0530

    adding all data into github repo

--- hideanddisplayform/myApp/src/app/newpage/newpage.page.ts
+++ hideanddisplayform/myApp/src/app/newpage/newpage.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-newpage',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewpagePage implements OnInit {
 
-  constructor() { }
+  oldId:any
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(event => {
+      // this.token = event.token;
+      // this.user_id = event.user_id;
+      console.log("here we get id",event.id);
+      this.oldId=event.id
+      console.log("here we store old ID",this.oldId);
+      
+      
+     });
+
+     this.abc()
+
+  }
+
+  abc(){
+    console.log("pass data as global",this.oldId);
+    
   }
 
 }
