a3f6836468207d0804ae1673cc363bb4241e4b99
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-01 23:36:54 +0530

    adding all data into gitlab repo

--- angular-project/firebasep/src/app/app.module.ts
+++ angular-project/firebasep/src/app/app.module.ts
@@ -4,14 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import * as firebase from 'firebase'
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDx25oDjWn84a3_HNRne4GT644Qo8a0eY",
+  authDomain: "auth-af7c9.firebaseapp.com",
+  projectId: "auth-af7c9",
+  storageBucket: "auth-af7c9.appspot.com",
+  messagingSenderId: "586866980229",
+  appId: "1:586866980229:web:b0b513994a7676cd8a80ec",
+  measurementId: "G-KRG0ZKZX3D"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
