9ce167a1f58fe9cf1be63555c89f17b9cb331558
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-01 23:54:42 +0530

    adding all file in gitlab repo

--- angular-project/firebasep/src/app/home/home.component.ts
+++ angular-project/firebasep/src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  [x: string]: any;
 
   otpSent: boolean = false
 phoneNumber = null
@@ -14,9 +17,33 @@ otp = null
   constructor() { }
 
   ngOnInit(): void {
+    this['recaptchaVerifier'] = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+      'size': 'invisible'
+      });
   }
 
-  sendOtp(){}
-signIn(){}
+  sendOtp(){
+    firebase.auth().signInWithPhoneNumber('+91' + this.phoneNumber, this['recaptchaVerifier'])
+
+    .then((confirmationResult) => {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      this['confirmationResult'] = confirmationResult;
+      this.otpSent = true;
+      }).catch(err => {
+      console.log(err)
+      })
+
+  }
+
+
+signIn(){
+
+  this['confirmationResult'].confirm(this.otp).then((user: any)=>{
+    console.log(user)
+    })
+}
 
 }
