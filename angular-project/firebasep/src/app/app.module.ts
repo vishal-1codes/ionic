9bc0d81c00e932ee1ba2d8be204546f57f41650d
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-01 23:23:33 +0530

    adding all data into database

--- angular-project/firebasep/src/app/app.module.ts
+++ angular-project/firebasep/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as firebase from 'firebase'
 
 @NgModule({
   declarations: [
