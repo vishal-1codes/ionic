b67e13c7ef44c26310564c7cd6e756f9c0eba279
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-22 21:45:15 +0530

    crete two new pages for login and register and test it

--- nodelogin/server.js
+++ nodelogin/server.js
@@ -8,4 +8,12 @@ app.get("/",(req,res)=>{
     res.render('index.ejs',{name:'vishal'})
 })
 
+app.get("/login",(req,res)=>{
+    res.render('login.ejs')
+})
+
+app.get("/register",(req,res)=>{
+    res.render('register.ejs')
+})
+
 app.listen(3000)
