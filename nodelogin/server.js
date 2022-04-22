2484c4820fa5bcbf8bc165357aaedac358fa9cf1
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-22 21:39:28 +0530

    pass data from server file to ejs

--- nodelogin/server.js
+++ nodelogin/server.js
@@ -1,8 +1,11 @@
 const express=require('express')
 const app=express()
 
+//here we set view engine to ejs
+app.set('view-engine','ejs')
+
 app.get("/",(req,res)=>{
-    res.render('index.ejs')
+    res.render('index.ejs',{name:'vishal'})
 })
 
 app.listen(3000)
