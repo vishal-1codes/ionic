238cbf0127a6fc0853ae7ef62fc8525732d7078d
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-22 22:05:19 +0530

    adding bcript library for hash password

--- nodelogin/server.js
+++ nodelogin/server.js
@@ -1,8 +1,15 @@
 const express=require('express')
 const app=express()
 
+//store all data in local variable
+const users=[]
+
+
 //here we set view engine to ejs
 app.set('view-engine','ejs')
+
+//we can access form data into req variable inside our post method
+app.use(express.urlencoded({extended:false}))
 
 app.get("/",(req,res)=>{
     res.render('index.ejs',{name:'vishal'})
@@ -12,15 +19,20 @@ app.get("/login",(req,res)=>{
     res.render('login.ejs')
 })
 
+//for login page
+app.post("/login",(req,res)=>{
+
+})
+
 app.get("/register",(req,res)=>{
     res.render('register.ejs')
 })
 
 
-//register part
+//for register page
 //next part
 app.post("/register",(req,res)=>{
-
+//that name field in input we access here after req.body
 })
 
 app.listen(3000)
