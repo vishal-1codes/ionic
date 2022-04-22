410bb5711824719b973b89de3c7374a6891b8b22
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-22 22:36:44 +0530

    add register data in array veriable

--- nodelogin/server.js
+++ nodelogin/server.js
@@ -1,5 +1,7 @@
 const express=require('express')
 const app=express()
+const bcrypt=require('bcrypt')
+const expressLayouts=require('express-ejs-layouts')
 
 //store all data in local variable
 const users=[]
@@ -7,6 +9,9 @@ const users=[]
 
 //here we set view engine to ejs
 app.set('view-engine','ejs')
+
+//ejs error solve
+// app.use(expressLayouts)
 
 //we can access form data into req variable inside our post method
 app.use(express.urlencoded({extended:false}))
@@ -31,8 +36,31 @@ app.get("/register",(req,res)=>{
 
 //for register page
 //next part
-app.post("/register",(req,res)=>{
+app.post("/register",async(req,res)=>{
 //that name field in input we access here after req.body
+//here we use try and catch asyncronous code
+try{
+    //create var , password input name field name , value of hash in number
+    const hashPassword=await bcrypt.hash(req.body.password,10)
+    //push code in users var that we create to store users
+    users.push({
+        //uniquely identify the value
+        id:Date.now().toString(),
+        name:req.body.name,
+        email:req.body.email,
+        //instend of using req.body.pasword we use hashpassword save to store in database
+        password:hashPassword
+    })
+    //if all ok then user redirect to login page
+    res.redirect("/login")
+
+}catch{
+    //if we get error , so user redirect to register page
+    res.redirect("/register")
+
+}
+//here we check user is add in array var or not
+console.log("here get psot data",users);
 })
 
 app.listen(3000)
