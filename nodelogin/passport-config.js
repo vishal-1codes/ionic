e874dbe8c5447f7c7664de4af7c062af4d37d04f
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-23 23:59:47 +0530

    add bcrypt for authenticate user

--- nodelogin/passport-config.js
+++ nodelogin/passport-config.js
@@ -1,12 +1,16 @@
-
-
 //here we use local strategy
 const LocalStrategy=require('passport-local').Strategy
+//here we check user password match with email
+const bcrypt=require(bcrypt)
 
 function initialize(passport){
     const authenticateUser=(email,password,done)=>{
         //get from login page
         const user =getUserByEmail(email)
+        //here we pass condition
+        if(user==null){
+            return done(null,false,{message:'No user with that email'})
+        }
     }
     passport.use(new LocalStrategy({usernameField:'email'}),authenticateUser)
     passport.serializeUser((user,done)=>{
