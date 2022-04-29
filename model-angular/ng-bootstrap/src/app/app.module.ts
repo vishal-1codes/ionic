8dfdf412e367e8023832ee267a65782a2e199f21
vishal-1codes <pawarvishal10k@gmail.com>
2022-04-29 20:00:24 +0530

    adding compnent in github

--- model-angular/ng-bootstrap/src/app/app.module.ts
+++ model-angular/ng-bootstrap/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbComponent } from './ngb/ngb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbComponent
   ],
   imports: [
     BrowserModule,
---
+++ model-angular/ng-bootstrap/src/app/ngb/ngb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngb',
+  templateUrl: './ngb.component.html',
+  styleUrls: ['./ngb.component.css']
+})
+export class NgbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
