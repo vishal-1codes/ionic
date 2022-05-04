bcb9fd79240f6315df1ee68367b905ea45fbafa5
vishal-1codes <pawarvishal10k@gmail.com>
2022-05-04 21:33:21 +0530

    adding next and prev button in slides

--- angular/ngx-slick/slides/src/app/slideone/slideone.component.ts
+++ angular/ngx-slick/slides/src/app/slideone/slideone.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { SlickCarouselComponent } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-slideone',
@@ -6,13 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slideone.component.css']
 })
 export class SlideoneComponent implements OnInit {
+  @ViewChild('slickModal') slickModal: SlickCarouselComponent;
+
+  next() {
+    this.slickModal.slickNext();
+  }
+  
+  prev() {
+    this.slickModal.slickPrev();
+  }
+  
   slides = [
     {img: "\https://images.unsplash.com/photo-1556189250-72ba954cfc2b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym13fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"},
     {img: "https://images.unsplash.com/flagged/photo-1553505192-acca7d4509be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Ym13fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"},
     {img: "https://images.unsplash.com/photo-1555215695-3004980ad54e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Ym13fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"},
     {img: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJtd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"}
   ];
-  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4};
+  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4,
+  speed: 2200,
+  arrows:false,
+  centerMode:true, 
+  cssEase: 'linear'};
 
   addSlide() {
     this.slides.push({img: "https://images.unsplash.com/photo-1556800572-1b8aeef2c54f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJtd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"})
