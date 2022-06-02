d37d63cbd4818b796b57712f9b3902d6c391868d
vishal-1codes <pawarvishal10k@gmail.com>
2022-06-02 23:08:46 +0530

    adding form control in html form

--- css/normal-css/angular-projects/forms/src/app/app.module.ts
+++ css/normal-css/angular-projects/forms/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormoneComponent } from './formone/formone.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { FormoneComponent } from './formone/formone.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- css/normal-css/angular-projects/forms/src/app/formone/formone.component.ts
+++ css/normal-css/angular-projects/forms/src/app/formone/formone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-formone',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormoneComponent implements OnInit {
 
+  contactSection = new FormGroup({
+    name: new FormControl('',Validators.required),
+    email: new FormControl('',Validators.required),
+    phonenumber: new FormControl('',Validators.required),
+    location: new FormControl('',Validators.required)
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  callingFunction(){
+    console.log("form data",this.contactSection);
+    
   }
 
 }
