97198c1a477216ff8c2a7a74bbab62435ece9365
vishal-1codes <pawarvishal10k@gmail.com>
2022-06-02 22:46:27 +0530

    cretae new component in gitlab repo

--- css/normal-css/angular-projects/forms/src/app/app.module.ts
+++ css/normal-css/angular-projects/forms/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormoneComponent } from './formone/formone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormoneComponent
   ],
   imports: [
     BrowserModule,
---
+++ css/normal-css/angular-projects/forms/src/app/formone/formone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formone',
+  templateUrl: './formone.component.html',
+  styleUrls: ['./formone.component.css']
+})
+export class FormoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
