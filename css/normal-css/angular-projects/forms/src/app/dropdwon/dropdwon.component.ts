07f32f783c13f54524e759198e41d9cce3d3e939
vishal-1codes <pawarvishal10k@gmail.com>
2022-06-14 22:16:55 +0530

    adding dropdown list in gitlab

--- css/normal-css/angular-projects/forms/src/app/dropdwon/dropdwon.component.ts
+++ css/normal-css/angular-projects/forms/src/app/dropdwon/dropdwon.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DropdwonComponent implements OnInit {
 
+  //variable section
+  newArray:any=[
+    {carname:'BMW',carModel:'BMW 1'},
+    {carname:'Audi',carModel:'Audi 1'},
+    {carname:'BBT',carModel:'BBT 1'},
+    {carname:'Mahindra',carModel:'Mahindra 1'},
+    {carname:'Tata',carModel:'Tata 1'},
+  ]
+
+  newBrands:any=[
+    {bName:'Aone'},
+    {bName:'Bone'},
+    {bName:'Cone'},
+    {bName:'Done'},
+    {bName:'Eone'},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  doSomething(value:any){
+    console.log("select call here",value);
+    
+    
   }
 
 }
