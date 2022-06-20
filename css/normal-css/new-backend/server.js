eccb6240a2a8085450585eb70b412b591a690a6b
vishal-1codes <pawarvishal10k@gmail.com>
2022-06-20 23:05:43 +0530

    successfuly connected to database

--- css/normal-css/new-backend/server.js
+++ css/normal-css/new-backend/server.js
@@ -1,17 +1,35 @@
+//here we get all install packages
 const express=require("express")
 const morgan=require("morgan")
 const mongoose=require("mongoose")
 const bodyParser=require("body-parser")
 
+//here we connected database
 mongoose.connect("mongodb+srv://m001-student:m001122@cluster0.34cov.mongodb.net/new-backend?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
 
+//Mongoose lets you start using your models immediately, without waiting for mongoose to establish a connection to MongoDB.
 const db=mongoose.connection
 
+//if mongodb connection get error
 db.on('error',(err)=>{
     console.log(err);
 })
 
+//mongodb connection get success message
 db.once('opne',()=>{
     console.log("connection successful....");
 })
 
+//here we use express with app variable
+const app=express()
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+//here we defiend port
+const PORT=process.env.PORT || 3000
+
+//connection message also we change package.json file for start server.js file
+app.listen(PORT,()=>{
+    console.log(`database successfully connected to port ${PORT}`);
+})
