54623fb1a367dec6fbee2d9b6840e94e0e3b29a7
vishal-1codes <pawarvishal10k@gmail.com>
2022-06-25 00:07:54 +0530

    add next and prev button in swiper js

--- css/normal-css/angular-projects/forms/src/app/swiper/swiper.component.ts
+++ css/normal-css/angular-projects/forms/src/app/swiper/swiper.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import Swiper from 'swiper';
+// import Swiper from 'swiper';
+// import SwiperCore from 'swiper';
+
+import SwiperCore, { SwiperOptions,Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+import 'swiper/less';
+import 'swiper/less/navigation';
+import 'swiper/less/pagination';
 
 
 @Component({
@@ -9,31 +17,57 @@ import Swiper from 'swiper';
 })
 export class SwiperComponent implements OnInit {
 
+   
+
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
+
   constructor() { }
 
   ngOnInit(): void {
-    const swiper = new Swiper('.swiper', {
-      // Optional parameters
-      //below two line coments because this give vertical line
-      // direction: 'vertical',
-      // loop: true,
+    // const swiper = new Swiper('.swiper', {
+    //   // Optional parameters
+    //   //below two line coments because this give vertical line
+    //   // direction: 'vertical',
+    //   // loop: true,
     
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
+    //   // If we need pagination
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //   },
     
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+    //   // Navigation arrows
+    //   navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    //   },
     
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
+    //   // And if we need scrollbar
+    //   scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //   },
+    // });
+
+   
+  }
+
+  // onSwiper(swiper:any) {
+  //   console.log(swiper);
+  // }
+  // onSlideChange() {
+  //   console.log('slide change');
+  // }
+
+  onSwiper(swiper:any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
   }
 
   
